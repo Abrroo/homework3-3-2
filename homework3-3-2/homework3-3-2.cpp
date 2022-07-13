ea3a462cbb02cf789c86df27547209fc6feb514e
Abrroo <akhmetov.ersyn@gmail.com>
2022-07-13 23:50:29 +0300

    delete method "game"; create func "game"; refact class "Counter"

--- homework3-3-2/homework3-3-2.cpp
+++ homework3-3-2/homework3-3-2.cpp
@@ -6,36 +6,44 @@ class Counter
 private:
     int x = 0;
 public:
-    int increment(int x) { this->x = this->x++; return this->x; }
-    int decrement(int x)   { this->x = this->x--; return this->x;}
-    void checkState(int x)  { this->x = this->x; std::cout << x << std::endl; }
-    Counter()              { this->x = 1; }
+    void increment() { ++x;}
+    void decrement()   { --x;}
+    int checkState() { return x; }
+    Counter()              { x = 1; }
     Counter(int x)         { this->x = x; }
-    void game()
-    {
-        char comcom = 0;
-        do
-        {
-            std::cout << "Введите команду('+', '-', '=' или 'x') :";
-            std::cin >> comcom;
-            switch (comcom)
-            {
-            case '+': increment(x); break;
-            case '-': decrement(x); break;
-            case '=': checkState(x); break;
-            case 'x': std::cout << "До свидания!"; break;
-            default: std::cout << "Нет такой команды! Еще раз!" << std::endl;
-            }
-
-        } while (comcom != 'x');
-    }
+    
 };
+
+void game(Counter res)
+{
+    char comcom = 0;
+    do
+    {
+
+        std::cout << "Введите команду('+', '-', '=' или 'x') :";
+        std::cin >> comcom;
+        switch (comcom)
+        {
+        case '+': res.increment(); break;
+        case '-': res.decrement(); break;
+        case '=':
+        {
+            std::cout << res.checkState() << std::endl;
+            break;
+        }
+        case 'x': std::cout << "До свидания!"; break;
+        default: std::cout << "Нет такой команды! Еще раз!" << std::endl;
+        }
+
+    } while (comcom != 'x');
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     int xx = 0;
     std::string ansans = "";
+    char comcom = 0;
     do
     {
         std::cout << "Вы хотите указать начальное значение счётчика? Введите yes или no: ";
@@ -43,7 +51,7 @@ int main()
         if (ansans == "no")
         {
             Counter res;
-            res.game();
+            game(res);
             
         }
         else if (ansans == "yes")
@@ -51,7 +59,7 @@ int main()
             std::cout << "Введите начальное значение счётчика: ";
             std::cin >> xx;
             Counter res(xx);
-            res.game();
+            game(res);
         }
         else
         {
